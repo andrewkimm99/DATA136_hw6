c6bdfb43788dbb11c2435a39721350e5cebe9fb6
andrewkimm99 <andrewkimm99@gmail.com>
2024-05-10 05:37:22 -0500

    Add files via upload

--- pa6.py
+++ pa6.py
@@ -1,6 +1,8 @@
 '''
 hw 6
 '''
+
+
 # Problem 1
 def make_change(total):
     '''
@@ -8,15 +10,18 @@ def make_change(total):
     '''
     coins = [1, 5, 10, 25, 100]
     combinations = []
+
     def change_combinations(total, combination, index):
         if total == 0:
             combinations.append(combination)
         else:
             for i in range(index, len(coins)):
                 if total - coins[i] >= 0:
-                    change_combinations(total - coins[i], combination + [coins[i]], i)
+                    change_combinations(total - coins[i],
+                                        combination + [coins[i]], i)
     change_combinations(total, [], 0)
     return combinations
+
 
 # Problem 2
 def dict_filter(f, d):
@@ -29,6 +34,7 @@ def dict_filter(f, d):
             return_dict[key] = value
     return return_dict
 
+
 # Problem 3
 def treemap(f, tree):
     '''
@@ -40,15 +46,21 @@ def treemap(f, tree):
     for child in tree.children:
         treemap(f, child)
 
+
 # Problem 4
 class DTree:
     def __init__(self, variable, threshold, lessequal, greater, outcome):
         if outcome is None:
-            if any([arg is None for arg in [variable, threshold, lessequal, greater]]):
-                raise ValueError('If outcome is none, other variables cannot be none')
+            if any([arg is None for arg in [variable,
+                                            threshold, lessequal, greater]]):
+                raise ValueError('If outcome is none, other\
+                                  variables cannot be none')
         else:
-            if any([arg is not None for arg in [variable, threshold, lessequal, greater]]):
-                raise ValueError('If outcome is not none, all first four arguments are none')
+            if any([arg is not None for arg in [variable,
+                                                threshold,
+                                                lessequal, greater]]):
+                raise ValueError('If outcome is not none,\
+                                  all first four arguments are none')
         self.variable = variable
         self.threshold = threshold
         self.lessequal = lessequal
@@ -56,6 +68,9 @@ class DTree:
         self.outcome = outcome
 
     def tuple_atleast(self):
+        '''
+        find the minimum length of tuple
+        '''
         def find_max(tree):
             if tree.variable is None:
                 return -1
@@ -72,6 +87,9 @@ class DTree:
         return rv
 
     def find_outcome(self, tup):
+        '''
+        find outcome based on given tuple
+        '''
         if self.outcome is not None:
             return self.outcome
         if tup[self.variable] <= self.threshold:
@@ -80,13 +98,16 @@ class DTree:
             return self.greater.find_outcome(tup)
 
     def no_repeats(self):
+        '''
+        find whether tree doesn't check same condition numerous times
+        '''
         def repeat_tracker(self, vars_l):
             if self.outcome is not None:
                 return True
             if self.variable in vars_l:
                 return False
             vars_l.append(self.variable)
-            return repeat_tracker(self.lessequal, vars_l) and repeat_tracker(self.greater, vars_l)
+            return repeat_tracker(self.lessequal, vars_l) and\
+                repeat_tracker(self.greater, vars_l)
         vars_l = []
         return repeat_tracker(self, vars_l)
-    
