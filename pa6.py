965699f385821c61b12e957c97eaab6f55a53f56
andrewkimm99 <andrewkimm99@gmail.com>
2024-05-10 05:27:58 -0500

    Add files via upload

--- pa6.py
+++ pa6.py
@@ -3,13 +3,16 @@ hw 6
 '''
 # Problem 1
 def make_change(total):
+    '''
+    gives all possible coin changes for total value
+    '''
     coins = [1, 5, 10, 25, 100]
     combinations = []
     def change_combinations(total, combination, index):
         if total == 0:
             combinations.append(combination)
         else:
-            for i in range(index, len(coins)):        
+            for i in range(index, len(coins)):
                 if total - coins[i] >= 0:
                     change_combinations(total - coins[i], combination + [coins[i]], i)
     change_combinations(total, [], 0)
@@ -17,6 +20,9 @@ def make_change(total):
 
 # Problem 2
 def dict_filter(f, d):
+    '''
+    filters dictionary values that return true for given function
+    '''
     return_dict = {}
     for key, value in d.items():
         if f(key, value):
@@ -25,6 +31,9 @@ def dict_filter(f, d):
 
 # Problem 3
 def treemap(f, tree):
+    '''
+    apply function to all nodes
+    '''
     new_key, new_value = f(tree.key, tree.value)
     tree.key = new_key
     tree.value = new_value
@@ -36,7 +45,7 @@ class DTree:
     def __init__(self, variable, threshold, lessequal, greater, outcome):
         if outcome is None:
             if any([arg is None for arg in [variable, threshold, lessequal, greater]]):
-                raise ValueError('If outcome is none, other variables cannot be none') 
+                raise ValueError('If outcome is none, other variables cannot be none')
         else:
             if any([arg is not None for arg in [variable, threshold, lessequal, greater]]):
                 raise ValueError('If outcome is not none, all first four arguments are none')
@@ -54,14 +63,14 @@ class DTree:
             if isinstance(tree.lessequal, DTree):
                 max_var = max(max_var, find_max(tree.lessequal))
             if isinstance(tree.greater, DTree):
-                max_var = max(max_var, find_max(tree.greater))                              
+                max_var = max(max_var, find_max(tree.greater))
             if max_var >= 0:
                 return max_var
             else:
                 return -1
         rv = find_max(self) + 1
         return rv
-        
+
     def find_outcome(self, tup):
         if self.outcome is not None:
             return self.outcome
@@ -69,7 +78,7 @@ class DTree:
             return self.lessequal.find_outcome(tup)
         else:
             return self.greater.find_outcome(tup)
-    
+
     def no_repeats(self):
         def repeat_tracker(self, vars_l):
             if self.outcome is not None:
@@ -80,16 +89,4 @@ class DTree:
             return repeat_tracker(self.lessequal, vars_l) and repeat_tracker(self.greater, vars_l)
         vars_l = []
         return repeat_tracker(self, vars_l)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
